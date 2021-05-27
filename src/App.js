04c314a4193003e8e099a37c3ff23dc25717e400
marchalseverine <81558460+marchalseverine@users.noreply.github.com>
2021-05-27 12:41:55 +0200

    Created React dictionary project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="head-title">
+    Dictionary React App
+    </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
