fc895fcb708b301e89dba51724b9cbc31a95cc0d
marchalseverine <81558460+marchalseverine@users.noreply.github.com>
2021-05-27 23:41:57 +0200

    Added Gallery Image Api + update design

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import Result from "./Result";
 import "./Dictionary.css";
+import Photos from "./Photos";
 import axios from "axios";
 
 export default function Dictionary (){
     let [keyWord, setKeyWord] = useState (null);
     let [result, setResult] = useState (null);
+    let [photos, setPhotos] = useState (null);
 
     function handleResponse (response){
         console.log(response.data);      
@@ -16,9 +18,18 @@ console.log(response.data[0].meanings[0].definitions[0].definition);
     function search (event){
         event.preventDefault();
         
+        function handlePexelsResponse(response){
+console.log(response.data);
+setPhotos(response.data.photos);
+        }
 
         let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`
         axios.get(apiUrl).then(handleResponse);
+
+const pexelsApiKey ="563492ad6f91700001000001ef1f242ed25b40a1aae0fb2214de5ec8";
+let pexelsApiUrl =`https://api.pexels.com/v1/search?query=${keyWord}&per_page=3`;
+let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
     function handleKeyWordChange (event){
        setKeyWord (event.target.value);
@@ -27,14 +38,15 @@ console.log(response.data[0].meanings[0].definitions[0].definition);
 
     return (
         <div className ="dictionary">
-<div className="form-box shadow">
-<h2 className="mb-4">What word do you want to look up?</h2>
+<div className="form-box shadow py-5">
+<h2>What word do you want to look up?</h2>
             <form className="search-engine" onSubmit ={search}>
                 <input type="search" placeholder="Type a word" autoFocus="on" onChange={handleKeyWordChange}/>
                 <input type ="submit" value="Search" />
             </form>
             </div>
             <Result result ={result}/>
+       <Photos photos={photos} />
         </div>
     )
 }
---
+++ src/Photos.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos (props){
+   console.log(props.photos);
+    
+    if (props.photos){
+         return (
+             <div className="photos shadow">
+                 
+                 <div className="row" >
+                 {props.photos.map(function(photo, index){
+                     return (
+                         <div key ={index} className="col-sm-3">
+                 <a href ={photo.src.original} target="_blank" rel="noreferrer">
+                         <img src= {photo.src.landscape} className="img-fluid" alt=" "/>
+                         </a>
+                         </div>
+                         
+                     )
+                 })}
+             </div>
+             </div>
+ 
+         )
+    } else {
+        return null;
+    }
+
+}
